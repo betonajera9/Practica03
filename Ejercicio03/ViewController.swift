3b20b0b0b5525bf6243d222d25df240e02755a69
betonajera9 <ing.betonajera@gmail.com>
2022-11-12 11:01:33 -0600

    :tada: begin: Initial excercise 03
    Initial excercise 03

--- Ejercicio03/ViewController.swift
+++ Ejercicio03/ViewController.swift
@@ -9,6 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBAction func TwitterBtn(_ sender: UIButton) {
+    }
+    @IBAction func FacebookBtn(_ sender: UIButton) {
+    }
+    @IBAction func YoutubeBtn(_ sender: Any) {
+    }
+    @IBAction func InstagramBtn(_ sender: Any) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
